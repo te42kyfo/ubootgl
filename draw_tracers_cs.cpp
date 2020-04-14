e7f01d99c904b8202c498dc3799050f841102ebf
Dominik Ernst <dominik.ernst@gmx.de>
2020-04-14 21:44:52 +0200

    CS based tracers: index and vertex buffer generation on GPU

--- draw_tracers_cs.cpp
+++ draw_tracers_cs.cpp
@@ -12,66 +12,84 @@ using vec2 = glm::vec2;
 namespace DrawTracersCS {
 GLuint tracer_shader, compute_shader;
 GLuint vao;
-GLuint tracer_shader_TM;
-GLuint ssbo_points, ssbo_vertices;
+GLuint uloc_tracer_shader_TM, uloc_compute_shader_npoints,
+    uloc_compute_shader_ntracers;
+GLuint ssbo_points, ssbo_vertices, ssbo_pointers, ssbo_ages, ssbo_indices;
 
 vector<unsigned int> indices;
 
-const int npoints = 40;
-const int ntracers = 2;
+const int npoints = 500;
+const int ntracers = 10;
 
 void init() {
 
   tracer_shader =
       loadShader("./tracercs.vert", "./color.frag", {{0, "in_Position"}});
-  tracer_shader_TM = glGetUniformLocation(tracer_shader, "TM");
+  uloc_tracer_shader_TM = glGetUniformLocation(tracer_shader, "TM");
 
   compute_shader = loadComputeShader("tracers.cs");
+  uloc_compute_shader_npoints = glGetUniformLocation(compute_shader, "npoints");
+  uloc_compute_shader_ntracers =
+      glGetUniformLocation(compute_shader, "ntracers");
 
   GL_CALL(glGenVertexArrays(1, &vao));
   glGenBuffers(1, &ssbo_points);
   glGenBuffers(1, &ssbo_vertices);
+  glGenBuffers(1, &ssbo_pointers);
+  glGenBuffers(1, &ssbo_ages);
+  glGenBuffers(1, &ssbo_indices);
 
+  // initialize points SSBO
   vector<vec2> pointdata;
   for (int t = 0; t < ntracers; t++) {
     for (int p = 0; p < npoints; p++) {
-      pointdata.push_back(
-          vec2(100 + p * 10.2, 100.0 + sin(p * 0.2) * 10.2 + t * 1.2));
+      pointdata.push_back(vec2(10 * t, 200));
     }
   }
   glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo_points);
   glBufferData(GL_SHADER_STORAGE_BUFFER, pointdata.size() * sizeof(vec2),
                pointdata.data(), GL_STATIC_DRAW);
 
+  // allocate vertices SSBO with zeros
   glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo_vertices);
   glBufferData(GL_SHADER_STORAGE_BUFFER,
                sizeof(glm::vec2) * npoints * ntracers * 2, NULL,
                GL_STATIC_DRAW);
 
-  // generate indices for triangle strip once
-  for (int t = 0; t < ntracers; t++) {
-    int p = 0;
-    indices.push_back(p * 2);
-    indices.push_back(p * 2 + 1);
-    indices.push_back(p * 2 + 2);
-    for (p = 1; p < npoints - 1; p++) {
-      indices.push_back(p * 2 - 2);
-      indices.push_back(p * 2);
-      indices.push_back(p * 2 + 1);
+  // allocate pointers SSBO with zeros
+  vector<int> pointers(ntracers, 0);
+  glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo_pointers);
+  glBufferData(GL_SHADER_STORAGE_BUFFER, pointers.size() * sizeof(int),
+               pointers.data(), GL_STATIC_DRAW);
 
-      indices.push_back(p * 2);
-      indices.push_back(p * 2 + 1);
-      indices.push_back(p * 2 + 2);
-    }
+  // allocate pointers SSBO with zeros
+  vector<float> ages(ntracers, 0.0);
+  glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo_ages);
+  glBufferData(GL_SHADER_STORAGE_BUFFER, ages.size() * sizeof(float),
+               ages.data(), GL_STATIC_DRAW);
 
-    indices.push_back(p * 2 - 2);
-    indices.push_back(p * 2);
-    indices.push_back(p * 2 + 1);
-  }
+  // allocate index buffer
+  glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo_indices);
+  glBufferData(GL_SHADER_STORAGE_BUFFER,
+               ntracers * (npoints - 1) * 6 * sizeof(int), NULL,
+               GL_STATIC_DRAW);
 };
 
 void updateTracers(float *vx, float *vy, float *flag, int nx, int ny, float dt,
-                   float pwidth){};
+                   float pwidth) {
+
+  // compute vertices
+  GL_CALL(glUseProgram(compute_shader));
+  GL_CALL(glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, ssbo_points));
+  GL_CALL(glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, ssbo_vertices));
+  GL_CALL(glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 2, ssbo_pointers));
+  GL_CALL(glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 3, ssbo_ages));
+  GL_CALL(glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 4, ssbo_indices));
+  GL_CALL(glUniform1i(uloc_compute_shader_npoints, npoints));
+  GL_CALL(glUniform1i(uloc_compute_shader_ntracers, ntracers));
+
+  GL_CALL(glDispatchCompute((ntracers - 1) / 256 + 1, 1, 1));
+};
 
 void draw(int nx, int ny, glm::mat4 PVM, float pwidth) {
 
@@ -80,24 +98,20 @@ void draw(int nx, int ny, glm::mat4 PVM, float pwidth) {
 
   GL_CALL(glBindVertexArray(vao));
 
-  // compute vertices
-  GL_CALL(glUseProgram(compute_shader));
-  GL_CALL(glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, ssbo_points));
-  GL_CALL(glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, ssbo_vertices));
-  GL_CALL(glDispatchCompute((npoints - 1) / 256 + 1, 1, 1));
-  glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
-
   // Use vertex SSBO as vertex attrib ponter
   GL_CALL(glBindBuffer(GL_ARRAY_BUFFER, ssbo_vertices));
   GL_CALL(glEnableVertexAttribArray(0));
   GL_CALL(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0));
 
   GL_CALL(glUseProgram(tracer_shader));
-  GL_CALL(
-      glUniformMatrix4fv(tracer_shader_TM, 1, GL_FALSE, glm::value_ptr(TM)));
+  GL_CALL(glUniformMatrix4fv(uloc_tracer_shader_TM, 1, GL_FALSE,
+                             glm::value_ptr(TM)));
 
-  GL_CALL(glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT,
-                         indices.data()));
+  GL_CALL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ssbo_indices));
+
+  glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
+  GL_CALL(glDrawElements(GL_TRIANGLES, ntracers * (npoints - 1) * 6,
+                         GL_UNSIGNED_INT, NULL));
 
   GL_CALL(glDisableVertexAttribArray(0));
 };
--- tracers.cs
+++ tracers.cs
@@ -1,13 +1,69 @@
 #version 430
 
+
+uniform int npoints;
+uniform int ntracers;
+
 layout( std430, binding=0 )  buffer P {vec2  points[];};
 layout( std430, binding=1 )  buffer V {vec2  vertices[];};
+layout( std430, binding=2 )  buffer PP {uint  pointers[];};
+layout( std430, binding=3 )  buffer A {float  ages[];};
+layout( std430, binding=4 )  buffer I {uint  indices [];};
 
 layout( local_size_x = 256) in;
 
 void main() {
+    const float width = 1.0;
     uint gid = gl_GlobalInvocationID.x;
 
-    vertices[gid] = vec2(100.0+ gid* 10.0, 100. + sin(gid)*10); // (gid * 10.0 + 100.0, 100.0 + 10* sin(gid *0.1));
+    if (gid >= ntracers) return;
 
+    uint base = gid * npoints;
+    uint curr = pointers[gid] % npoints;
+    uint next = (pointers[gid] +1) % npoints;
+    uint prev = (pointers[gid] + npoints - 1) % npoints;
+
+    vec2 currP = points[base + curr];
+    points[base + next] = vec2(100+gid*350, 200) + vec2(sin(ages[gid]*(20.4 + gid*1.1)), cos(ages[gid]*1.2)) * (gid + 150.0);
+
+
+    vec2 v1 = normalize(points[base + curr]- points[base + prev]);
+    vec2 v2 =  normalize(points[base + next]- points[base + curr ]);
+
+    vec2 dir;
+    vec2 perp;
+
+
+    perp = vec2(v2.y, -v2.x);
+    vertices[base*2 + next*2] = points[base + next] + perp* width * 0.5;
+    vertices[base*2 + next*2 + 1]  = points[base + next] - perp* width * 0.5;
+
+
+    dir = 0.5* (v1 + v2);
+    perp = vec2(dir.y, -dir.x);
+    perp /=   0.1 + 0.9* dot(perp, vec2(v2.y, -v2.x));
+
+    vertices[base*2 + curr*2] = points[base + curr] + perp* width;
+    vertices[base*2 + curr*2 + 1] = points[base + curr] - perp* width;
+
+    // dir = normalize(points[(next + 2) % npoints] - points[(next + 1) % npoints]);
+    // perp = vec2(dir.y, -dir.x);
+    // vertices[base*2 + ((next+1)%npoints)*2] = points[base + (next+1) % npoints] + perp* width * 0.5;
+    // vertices[base*2 + ((next+1)%npoints)*2 + 1]  = points[base + (next+1) % npoints] - perp* width * 0.5;
+
+    uint ibase = gid * (npoints -1 )  * 6;
+    uint vbase = (gid * npoints) * 2;
+
+
+
+    indices[ibase + ((pointers[gid] + 1)% (npoints-1))*6 +  0] = vbase + (curr * 2 + 0) % (npoints*2);
+    indices[ibase + ((pointers[gid] + 1)% (npoints-1))*6 +  1] = vbase + (curr * 2 + 1) % (npoints*2);
+    indices[ibase + ((pointers[gid] + 1)% (npoints-1))*6 +  2] = vbase + (curr * 2 + 2) % (npoints*2);
+    indices[ibase + ((pointers[gid] + 1)% (npoints-1))*6 +  3] = vbase + (curr * 2 + 1) % (npoints*2);
+    indices[ibase + ((pointers[gid] + 1)% (npoints-1))*6 +  4] = vbase + (curr * 2 + 2) % (npoints*2);
+    indices[ibase + ((pointers[gid] + 1)% (npoints-1))*6 +  5] = vbase + (curr * 2 + 3) % (npoints*2);
+
+
+    ages [gid] = ages[gid] + 0.002;
+    pointers[gid] = pointers[gid] + 1;
 }
