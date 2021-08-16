a29b3763ec3d8f69fb28faf6581ebf499fac8460
Dominik Ernst <dominik.ernst@gmx.de>
2021-08-16 16:26:35 +0200

    swarm: evade torpedos, explosions

--- swarm.cpp
+++ swarm.cpp
@@ -8,10 +8,13 @@
 using namespace std;
 
 void classicSwarmAI(entt::registry &registry, const Single2DGrid &flag,
-                    const DoubleBuffered2DGrid &vx, const DoubleBuffered2DGrid &vy, float h) {
+                    const DoubleBuffered2DGrid &vx,
+                    const DoubleBuffered2DGrid &vy, float h) {
 
   auto agentView = registry.view<CoAgent, CoItem, CoKinematics>();
+  int agentNumber = 0;
   agentView.each([&](auto &item1, auto &kin1) {
+    agentNumber++;
     glm::vec2 swarmCenter = {0, 0};
     int swarmCount = 0;
     glm::vec2 rejectionDirection = {};
@@ -28,35 +31,88 @@ void classicSwarmAI(entt::registry &registry, const Single2DGrid &flag,
         continue;
       if (d < 10.0f * (item1.size.x + item1.size.y)) {
         swarmCenter += item2.pos + kin2.vel * 0.1f;
-        commonDirection += kin2.vel;
+        commonDirection += (length(kin2.vel) > 0.0f) ? normalize(kin2.vel) : glm::vec2(0.0f, 0.0f);
         swarmCount++;
       }
       if (d < (item1.size.x + item1.size.y) * 1.0f) {
         rejectionDirection +=
-            (item1.pos + kin1.vel * 0.01f - item2.pos - kin2.vel * 0.01f) / max(0.001f, d);
+            (item1.pos + kin1.vel * 0.01f - item2.pos - kin2.vel * 0.01f) /
+            max(0.001f, d);
       }
     }
-    swarmCount = max(swarmCount,1);
+    swarmCount = max(swarmCount, 1);
     auto lookAheadPos = item1.pos + kin1.vel * 0.02f;
-    glm::vec2 wallAvoidance  = glm::vec2( bilinearSample(flag, lookAheadPos / h - glm::vec2(4,0)) -
-                               bilinearSample(flag, lookAheadPos / h + glm::vec2(4,0)),
-                               bilinearSample(flag, lookAheadPos / h - glm::vec2(0,4)) -
-                               bilinearSample(flag, lookAheadPos / h + glm::vec2(0,4)));
-    wallAvoidance += glm::vec2( bilinearSample(flag,item1.pos / h - glm::vec2(4,0)) -
-                               bilinearSample(flag, item1.pos / h + glm::vec2(4,0)),
-                               bilinearSample(flag, item1.pos / h - glm::vec2(0,4)) -
-                                bilinearSample(flag, item1.pos / h + glm::vec2(0,4)));
+    glm::vec2 wallAvoidance =
+        glm::vec2(bilinearSample(flag, lookAheadPos / h - glm::vec2(4, 0)) -
+                      bilinearSample(flag, lookAheadPos / h + glm::vec2(4, 0)),
+                  bilinearSample(flag, lookAheadPos / h - glm::vec2(0, 4)) -
+                      bilinearSample(flag, lookAheadPos / h + glm::vec2(0, 4)));
+    wallAvoidance +=
+        glm::vec2(bilinearSample(flag, item1.pos / h - glm::vec2(4, 0)) -
+                      bilinearSample(flag, item1.pos / h + glm::vec2(4, 0)),
+                  bilinearSample(flag, item1.pos / h - glm::vec2(0, 4)) -
+                      bilinearSample(flag, item1.pos / h + glm::vec2(0, 4)));
 
     auto vgridPos = glm::ivec2((item1.pos / h));
-    auto flowDir = -glm::vec2(bilinearSample(vx, vgridPos), bilinearSample(vy, vgridPos));
+    auto flowDir =
+        -glm::vec2(bilinearSample(vx, vgridPos), bilinearSample(vy, vgridPos));
+
+    glm::vec2 playerDir = {0.0f, 0.0f};
+    int playerCount = 0;
+    registry.view<CoPlayer, CoItem, CoKinematics>().each(
+        [&](auto playerEnt, auto player, auto playerItem, auto playerKin) {
+          float d = length(item1.pos - playerItem.pos);
+          if (d < 10.0f * (item1.size.x + item1.size.y) && d > 0.01f) {
+            playerDir += normalize(item1.pos + kin1.vel * 0.02f -
+                                   playerItem.pos - playerKin.vel * 0.02f);
+          }
+        });
+    playerDir = playerDir / max((float)playerCount, 1.0f);
+
+    glm::vec2 torpedoDir = {0.0f, 0.0f};
+    float torpedoDist = -1.0f;
+    registry.view<CoTorpedo, CoItem, CoKinematics>().each(
+        [&](auto torpedo, auto torpedoItem, auto torpedoKin) {
+          float d =
+              length(item1.pos - torpedoItem.pos - torpedoKin.vel * 0.03f);
+          if (d < 7.0f * (item1.size.x + item1.size.y) &&
+              (d < torpedoDist || torpedoDist < 0)) {
+            torpedoDist = d;
+            torpedoDir = normalize(item1.pos + kin1.vel * 0.02f -
+                                   torpedoItem.pos - torpedoKin.vel * 0.02f);
+          }
+        });
+
+    glm::vec2 expDir = {0.0f, 0.0f};
+    float expDist = -1.0f;
+    registry.view<CoExplosion, CoItem>().each([&](auto exp, auto expItem) {
+      float d = length(item1.pos - expItem.pos);
+      if (d < 2.0f * (item1.size.x + item1.size.y) &&
+          (d < expDist || expDist < 0)) {
+        expDist = d;
+        expDir = normalize(item1.pos + kin1.vel * 0.02f - expItem.pos);
+      }
+    });
 
     auto targetDir =
         flowDir * 10.0f - wallAvoidance * 20.0f +
         (swarmCenter / (float)swarmCount - (item1.pos + kin1.vel * 0.01f)) *
             30.0f +
-        1.0f * rejectionDirection +
-        10.0f * commonDirection / (float)swarmCount;
+        10.0f * rejectionDirection +
+        2.0f * commonDirection / (float)swarmCount + playerDir * 1.0f +
+        expDir * 500.0f;
 
+
+
+    if (length(torpedoDir) > 0.1)
+      targetDir =  glm::rotate(torpedoDir, glm::sign(orientedAngle(torpedoDir, targetDir)) * -0.4f * 3.141f)*20.0f;
+
+    if (item1.pos.x < 0.1)
+      targetDir += glm::vec2(2.0f, 0.0f);
+    else
+      targetDir += glm::vec2(-1.0f, 0.0f);
+
+    
     float directedAngle = 0.0f;
     if (glm::length(targetDir) > 0.0f) {
       auto heading = glm::vec2{cos(item1.rotation), sin(item1.rotation)};
@@ -64,13 +120,14 @@ void classicSwarmAI(entt::registry &registry, const Single2DGrid &flag,
     }
 
     if (directedAngle < 0)
-        item1.rotation -= max(directedAngle, -0.2f);
+      item1.rotation -= max(directedAngle, -0.2f);
     else
-        item1.rotation -= min(directedAngle, +0.2f);
+      item1.rotation -= min(directedAngle, +0.2f);
 
-    if ( length(targetDir) < 0.1f) targetDir = glm::vec2(1.0, 0.0);
+    if (length(targetDir) < 0.1f)
+      targetDir = glm::vec2(1.0, 0.0);
 
-    kin1.force =  normalize(targetDir) * min(8.0f, max(2.0f, length(targetDir)));
+    kin1.force = normalize(targetDir) * (min(6.0f, max(0.0f, length(targetDir))));
     assert(!std::isnan(kin1.force.x));
     assert(!std::isnan(kin1.force.y));
   });
