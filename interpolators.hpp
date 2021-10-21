44ef90aaf018928a31a4e9e834d4438200462afa
Dominik Ernst <dominik.ernst@gmx.de>
2021-10-21 18:50:43 +0200

    advection: use SSE loads instead of gather

--- interpolators.hpp
+++ interpolators.hpp
@@ -1,8 +1,12 @@
 #ifndef __INTERPOLATORS_H_
 #define __INTERPOLATORS_H_
 
+
 #include "db2dgrid.hpp"
 #include <glm/glm.hpp>
+#include <immintrin.h>
+
+#include "immintrin.h"
 
 template <typename GridType>
 inline float bilinearSample(const GridType &grid, glm::vec2 c) {
@@ -80,6 +84,11 @@ inline __m256 CubicHermite(__m256 t, __m256 A, __m256 B, __m256 C, __m256 D) {
   return a * t * t * t + b * t * t + c * t + d;
 }
 
+
+
+
+
+
 template <typename GridType>
 inline __m256 bicubicSample(const GridType &grid, __m256 cx, __m256 cy) {
   cx = _mm256_max_ps(_mm256_min_ps(cx, _mm256_set1_ps(grid.width - 3.0f)),
@@ -93,45 +102,130 @@ inline __m256 bicubicSample(const GridType &grid, __m256 cx, __m256 cy) {
   __m256 stx = _mm256_sub_ps(cx, _mm256_round_ps(cx, _MM_FROUND_TRUNC));
   __m256 sty = _mm256_sub_ps(cy, _mm256_round_ps(cy, _MM_FROUND_TRUNC));
 
-  __m256 v11 = gridGather(grid, _mm256_add_epi32(icx, _mm256_set1_epi32(-1)),
-                          _mm256_add_epi32(icy, _mm256_set1_epi32(-1)));
-  __m256 v12 = gridGather(grid, _mm256_add_epi32(icx, _mm256_set1_epi32(0)),
-                          _mm256_add_epi32(icy, _mm256_set1_epi32(-1)));
-  __m256 v13 = gridGather(grid, _mm256_add_epi32(icx, _mm256_set1_epi32(1)),
-                          _mm256_add_epi32(icy, _mm256_set1_epi32(-1)));
-  __m256 v14 = gridGather(grid, _mm256_add_epi32(icx, _mm256_set1_epi32(2)),
-                          _mm256_add_epi32(icy, _mm256_set1_epi32(-1)));
 
-  __m256 v21 = gridGather(grid, _mm256_add_epi32(icx, _mm256_set1_epi32(-1)),
-                          _mm256_add_epi32(icy, _mm256_set1_epi32(0)));
-  __m256 v22 = gridGather(grid, _mm256_add_epi32(icx, _mm256_set1_epi32(0)),
-                          _mm256_add_epi32(icy, _mm256_set1_epi32(0)));
-  __m256 v23 = gridGather(grid, _mm256_add_epi32(icx, _mm256_set1_epi32(1)),
-                          _mm256_add_epi32(icy, _mm256_set1_epi32(0)));
-  __m256 v24 = gridGather(grid, _mm256_add_epi32(icx, _mm256_set1_epi32(2)),
-                          _mm256_add_epi32(icy, _mm256_set1_epi32(0)));
 
-  __m256 v31 = gridGather(grid, _mm256_add_epi32(icx, _mm256_set1_epi32(-1)),
-                          _mm256_add_epi32(icy, _mm256_set1_epi32(1)));
-  __m256 v32 = gridGather(grid, _mm256_add_epi32(icx, _mm256_set1_epi32(0)),
-                          _mm256_add_epi32(icy, _mm256_set1_epi32(1)));
-  __m256 v33 = gridGather(grid, _mm256_add_epi32(icx, _mm256_set1_epi32(1)),
-                          _mm256_add_epi32(icy, _mm256_set1_epi32(1)));
-  __m256 v34 = gridGather(grid, _mm256_add_epi32(icx, _mm256_set1_epi32(2)),
-                          _mm256_add_epi32(icy, _mm256_set1_epi32(1)));
+  __m256i x = _mm256_add_epi32(icx, _mm256_set1_epi32(-1));
+  __m256i y = _mm256_add_epi32(icy, _mm256_set1_epi32(-1));
+  __m256i idx =
+    _mm256_add_epi32(x, _mm256_mullo_epi32(y, _mm256_set1_epi32(grid.width)));
 
-  __m256 v41 = gridGather(grid, _mm256_add_epi32(icx, _mm256_set1_epi32(-1)),
-                          _mm256_add_epi32(icy, _mm256_set1_epi32(2)));
-  __m256 v42 = gridGather(grid, _mm256_add_epi32(icx, _mm256_set1_epi32(0)),
-                          _mm256_add_epi32(icy, _mm256_set1_epi32(2)));
-  __m256 v43 = gridGather(grid, _mm256_add_epi32(icx, _mm256_set1_epi32(1)),
-                          _mm256_add_epi32(icy, _mm256_set1_epi32(2)));
-  __m256 v44 = gridGather(grid, _mm256_add_epi32(icx, _mm256_set1_epi32(2)),
-                          _mm256_add_epi32(icy, _mm256_set1_epi32(2)));
+  __m128 l0 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 0));
+  __m128 l1 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 1));
+  __m128 l2 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 2));
+  __m128 l3 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 3));
+  __m128 l4 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 4));
+  __m128 l5 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 5));
+  __m128 l6 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 6));
+  __m128 l7 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 7));
+
+  __m256 A0 = _mm256_insertf128_ps(_mm256_castps128_ps256(l0), l4, 1);
+  __m256 A1 = _mm256_insertf128_ps(_mm256_castps128_ps256(l1), l5, 1);
+  __m256 A2 = _mm256_insertf128_ps(_mm256_castps128_ps256(l2), l6, 1);
+  __m256 A3 = _mm256_insertf128_ps(_mm256_castps128_ps256(l3), l7, 1);
+
+  __m256 t1 = _mm256_unpacklo_ps(A0, A1);
+  __m256 t2 = _mm256_unpacklo_ps(A2, A3);
+  __m256 t3 = _mm256_unpackhi_ps(A0, A1);
+  __m256 t4 = _mm256_unpackhi_ps(A2, A3);
+
+  __m256 v11 = _mm256_shuffle_ps(t1, t2, 0b01000100);
+  __m256 v12 = _mm256_shuffle_ps(t1, t2, 0b11101110);
+  __m256 v13 = _mm256_shuffle_ps(t3, t4, 0b01000100);
+  __m256 v14 = _mm256_shuffle_ps(t4, t4, 0b11101110);
 
   __m256 v1 = CubicHermite(stx, v11, v12, v13, v14);
+
+  x = _mm256_add_epi32(icx, _mm256_set1_epi32(-1));
+  y = _mm256_add_epi32(icy, _mm256_set1_epi32(0));
+  idx =
+    _mm256_add_epi32(x, _mm256_mullo_epi32(y, _mm256_set1_epi32(grid.width)));
+
+   l0 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 0));
+   l1 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 1));
+   l2 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 2));
+   l3 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 3));
+   l4 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 4));
+   l5 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 5));
+   l6 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 6));
+   l7 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 7));
+
+   A0 = _mm256_insertf128_ps(_mm256_castps128_ps256(l0), l4, 1);
+   A1 = _mm256_insertf128_ps(_mm256_castps128_ps256(l1), l5, 1);
+   A2 = _mm256_insertf128_ps(_mm256_castps128_ps256(l2), l6, 1);
+   A3 = _mm256_insertf128_ps(_mm256_castps128_ps256(l3), l7, 1);
+
+   t1 = _mm256_unpacklo_ps(A0, A1);
+   t2 = _mm256_unpacklo_ps(A2, A3);
+   t3 = _mm256_unpackhi_ps(A0, A1);
+   t4 = _mm256_unpackhi_ps(A2, A3);
+
+  __m256 v21 = _mm256_shuffle_ps(t1, t2, 0b01000100);
+  __m256 v22 = _mm256_shuffle_ps(t1, t2, 0b11101110);
+  __m256 v23 = _mm256_shuffle_ps(t3, t4, 0b01000100);
+  __m256 v24 = _mm256_shuffle_ps(t4, t4, 0b11101110);
+
   __m256 v2 = CubicHermite(stx, v21, v22, v23, v24);
+
+  x = _mm256_add_epi32(icx, _mm256_set1_epi32(-1));
+  y = _mm256_add_epi32(icy, _mm256_set1_epi32(1));
+  idx =
+    _mm256_add_epi32(x, _mm256_mullo_epi32(y, _mm256_set1_epi32(grid.width)));
+
+   l0 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 0));
+   l1 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 1));
+   l2 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 2));
+   l3 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 3));
+   l4 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 4));
+   l5 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 5));
+   l6 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 6));
+   l7 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 7));
+
+   A0 = _mm256_insertf128_ps(_mm256_castps128_ps256(l0), l4, 1);
+   A1 = _mm256_insertf128_ps(_mm256_castps128_ps256(l1), l5, 1);
+   A2 = _mm256_insertf128_ps(_mm256_castps128_ps256(l2), l6, 1);
+   A3 = _mm256_insertf128_ps(_mm256_castps128_ps256(l3), l7, 1);
+
+   t1 = _mm256_unpacklo_ps(A0, A1);
+   t2 = _mm256_unpacklo_ps(A2, A3);
+   t3 = _mm256_unpackhi_ps(A0, A1);
+   t4 = _mm256_unpackhi_ps(A2, A3);
+
+  __m256 v31 = _mm256_shuffle_ps(t1, t2, 0b01000100);
+  __m256 v32 = _mm256_shuffle_ps(t1, t2, 0b11101110);
+  __m256 v33 = _mm256_shuffle_ps(t3, t4, 0b01000100);
+  __m256 v34 = _mm256_shuffle_ps(t4, t4, 0b11101110);
+
   __m256 v3 = CubicHermite(stx, v31, v32, v33, v34);
+
+  x = _mm256_add_epi32(icx, _mm256_set1_epi32(-1));
+  y = _mm256_add_epi32(icy, _mm256_set1_epi32(2));
+  idx =
+    _mm256_add_epi32(x, _mm256_mullo_epi32(y, _mm256_set1_epi32(grid.width)));
+
+   l0 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 0));
+   l1 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 1));
+   l2 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 2));
+   l3 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 3));
+   l4 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 4));
+   l5 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 5));
+   l6 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 6));
+   l7 = _mm_loadu_ps( grid.data() + _mm256_extract_epi32(idx, 7));
+
+   A0 = _mm256_insertf128_ps(_mm256_castps128_ps256(l0), l4, 1);
+   A1 = _mm256_insertf128_ps(_mm256_castps128_ps256(l1), l5, 1);
+   A2 = _mm256_insertf128_ps(_mm256_castps128_ps256(l2), l6, 1);
+   A3 = _mm256_insertf128_ps(_mm256_castps128_ps256(l3), l7, 1);
+
+   t1 = _mm256_unpacklo_ps(A0, A1);
+   t2 = _mm256_unpacklo_ps(A2, A3);
+   t3 = _mm256_unpackhi_ps(A0, A1);
+   t4 = _mm256_unpackhi_ps(A2, A3);
+
+  __m256 v41 = _mm256_shuffle_ps(t1, t2, 0b01000100);
+  __m256 v42 = _mm256_shuffle_ps(t1, t2, 0b11101110);
+  __m256 v43 = _mm256_shuffle_ps(t3, t4, 0b01000100);
+  __m256 v44 = _mm256_shuffle_ps(t4, t4, 0b11101110);
+
   __m256 v4 = CubicHermite(stx, v41, v42, v43, v44);
 
   return CubicHermite(sty, v1, v2, v3, v4);
